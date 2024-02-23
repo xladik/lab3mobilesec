b91d8b08936fa2b492102051a6c78a7404c3bd86
Adik <adilet1155577@gmail.com>
2024-02-23 11:05:20 +0600

    lab3

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sis2125_lab3_Adilet_Chaikenov"
+include(":app")
+ 
